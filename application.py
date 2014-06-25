6435e1960e6c7218563bd7030220bcfd941d848a
Jake Swanson <jakswa@gmail.com>
2014-06-25 16:15:35 -0400

    readline typo, comments

--- application.py
+++ application.py
@@ -1,11 +1,14 @@
 from flask import Flask
+# interface with R
 import rpy2.robjects as robjects
+
 application = Flask(__name__)
 
 application.debug=True
 
 @application.route("/")
 def hello():
+    # running some arbitrary R code to test
     str = robjects.r('paste("IM", "A STRING", "BUILT WITH RRRRR")')[0]
     return "Hello world! " + str
 

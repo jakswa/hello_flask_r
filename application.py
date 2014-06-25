188a8a6b43b7343f2077a7814396811418783a71
Jake Swanson <jakswa@gmail.com>
2014-06-25 14:52:11 -0400

    here we go, testing some R code...

--- application.py
+++ application.py
@@ -1,11 +1,13 @@
 from flask import Flask
+import rpy2.robjects as robjects
 application = Flask(__name__)
 
 application.debug=True
 
 @application.route("/")
 def hello():
-    return "Hello world!"
+    str = robjects.r('paste("IM", "A STRING", "BUILT WITH RRRRR")')[0]
+    return "Hello world! " + str
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', debug=True)
